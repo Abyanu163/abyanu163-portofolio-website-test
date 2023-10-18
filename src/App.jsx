78066c933774e95c9494d077b4f261de08c6b5f2
Abyanu163 <127492205+Abyanu163@users.noreply.github.com>
2023-10-18 01:28:33 +0000

    set homepage for github actions pages

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Abyanu163",
+  title: "Student and Content archiver",
+  email: "abyanu@1423.my.id",
+  gitHub: "Abyanu163",
+  instagram: "",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
